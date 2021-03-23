d7d85c1be8c822ac56ed7d87543509bae42be850
manipabba <35638577+manipabba@users.noreply.github.com>
2021-03-23 02:59:07 -0400

    add new JavaDoc version

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"GUI","c":"ValveGUI","l":"initDefaults()"},{"p":"GUI","c":"ValveGUI","l":"menuBarSetup()"},{"p":"GUI","c":"ValveGUI","l":"parseSetup()"},{"p":"GUI","c":"ValveGUI","l":"ValveGUI(String, String)","url":"%3Cinit%3E(java.lang.String,java.lang.String)"}]
