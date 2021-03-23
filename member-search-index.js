7925829f479ecab703cff2bae1f728013587e840
manipabba <35638577+manipabba@users.noreply.github.com>
2021-03-23 03:01:01 -0400

    empty again

--- member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"GUI","c":"ValveGUI","l":"initDefaults()"},{"p":"GUI","c":"ValveGUI","l":"menuBarSetup()"},{"p":"GUI","c":"ValveGUI","l":"parseSetup()"},{"p":"GUI","c":"ValveGUI","l":"ValveGUI(String, String)","url":"%3Cinit%3E(java.lang.String,java.lang.String)"}]
